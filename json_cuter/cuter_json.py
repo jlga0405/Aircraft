2d08f7628a18d4c26bd1fcde6ab7717d6cb09e8d
Jorge Garcia A <165052250+jlga0405@users.noreply.github.com>
2024-04-11 20:17:49 -0300

    Act_jsoncutter

--- json_cuter/cuter_json.py
+++ json_cuter/cuter_json.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-def split_json_file(file_path, chunk_size):
+def split_json_file(file_path, chunk_size, output_base_name):
     with open(file_path, 'r') as file:
         data = json.load(file)
 
@@ -12,17 +12,18 @@ def split_json_file(file_path, chunk_size):
         end_idx = start_idx + chunk_size
 
         chunk_data = data[start_idx:end_idx]
-        chunk_file_name = f"data_list{i + 1:02d}.json"
+        chunk_file_name = f"{output_base_name}_{i + 1:02d}.json"  # Nombre de archivo basado en el nombre base y el contador
 
         with open(chunk_file_name, 'w') as chunk_file:
-            json.dump(chunk_data, chunk_file, indent=2)  # Serializa cada parte con sangría
+            json.dump(chunk_data, chunk_file, indent=2)
 
 if __name__ == "__main__":
     json_file_path = "data.json"
+    output_base_name = "Lista_WENCOR_correr"  # Nombre base para los archivos de salida
     chunk_size = int(input("Ingrese el tamaño de cada parte: "))
 
     if not os.path.exists(json_file_path):
         print(f"El archivo {json_file_path} no existe.")
     else:
-        split_json_file(json_file_path, chunk_size)
+        split_json_file(json_file_path, chunk_size, output_base_name)
         print("El archivo se ha dividido en partes correctamente.")
