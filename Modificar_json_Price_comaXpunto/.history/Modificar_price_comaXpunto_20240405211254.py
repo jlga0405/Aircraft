6d234d3a3d967d67ec3d183af296594638700027
Jorge Luis Garcia <jorgexgarcia1@hotmail.com>
2024-10-10 22:17:05 -0300

    act

--- Modificar_json_Price_comaXpunto/.history/Modificar_price_comaXpunto_20240405211254.py
+++ Modificar_json_Price_comaXpunto/.history/Modificar_price_comaXpunto_20240405211254.py
@@ -1,13 +1,13 @@
 import json
 
 # Lee el archivo JSON
-with open('List_airfasco_Act_10102024.json', 'r') as f:
+with open('Act_Lonseal_precio_26032024.json', 'r') as f:
     data = json.load(f)
 
 # Itera sobre cada objeto y modifica el valor de productprice
 for product in data:
     # Convierte el valor de productprice a una cadena y luego realiza el reemplazo
-    product['productweight'] = str(product['productweight']).replace(',', '.')
+    product['productprice'] = str(product['productprice']).replace(',', '.')
 
 # Guarda el JSON modificado en un nuevo archivo
 with open('archivo_modificado.json', 'w') as f:
